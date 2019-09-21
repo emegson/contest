ca5cf45187aa26e4060018ee3eb71a11b984ee65
Emerson Almeida Matos <emegson@live.com>
2019-09-21 12:56:48 -0300

    1020.c

---
+++ URI/C/10xx/1020.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//problema 1020;
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    printf("valor = %d\n", n);
+    
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", n/365>0?n/365:0,(n%365)>0?(n%365)/30:0,(n%365)%30);
+    
+    return 0;
+}
