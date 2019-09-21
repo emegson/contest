2a4455aaf784db7d09ff01ea8d48f294265d7fd3
Emerson Almeida Matos <emegson@live.com>
2019-09-21 13:17:17 -0300

    1021

--- URI/C/10xx/1021.c
+++ URI/C/10xx/1021.c
@@ -9,10 +9,9 @@ int main()
     int n, i;
     float cedulas[6] = {100.00, 50.00, 20.00, 10.00, 5.00, 2.00}; 
     double moedas[6] = {1.00, 0.50, 0.25, 0.10, 0.05, 0.01};
-    char cedulaString[15] = {" nota(s) de R$ "}, moedaString[16] = {" moeda(s) de R$"};
+    char cedulaString[] = " nota(s) de R$ ", moedaString[] = " moeda(s) de R$";
 
     scanf("%lf", &valor);
-    //printf("valor = %lf\n", valor);
     printf("NOTAS:\n");
     
     for(i=0, n=6; i<n; i++)
@@ -21,10 +20,11 @@ int main()
         valor = valor - ((int)(valor/(cedulas[i]))*cedulas[i]);
     }
     printf("MOEDAS:\n");
-    for(i=0, n=6; i<n; i++)
+    for(i=0, n=5; i<n; i++)
     {   //NOTAS
         printf("%d%s %.2f\n", (int)(valor/(moedas[i])), moedaString, moedas[i]);
         valor = valor - ((int)(valor/(moedas[i])) * moedas[i]);
     }
+    printf("%.f%s %.2f\n", (valor * 100), moedaString, moedas[i]);
     return 0;
 }
